c1974550f0148e347d0aaa0d7e75173c5d5b0ccc
rahulgidde.com <rahulgidde1212@gmail.com>
2020-04-26 23:09:49 +0530

    Added:Extend The Servlet To Accept Valid Name

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -7,30 +7,34 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.regex.Pattern;
 
 @WebServlet(
         description = "Login Servlet Testing",
         urlPatterns = {"/LoginServlet"},
         initParams = {
+                @WebInitParam(name = "namePattern", value = "^[A-Z][A-Za-z0-9]{2,}$"),
                 @WebInitParam(name = "user", value = "Rahul"),
                 @WebInitParam(name = "password", value = "123")
         }
 )
 public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String name = request.getParameter("name");
         String user = request.getParameter("user");
         String password = request.getParameter("pwd");
 
+        String userName = getServletConfig().getInitParameter("namePattern");
         String userId = getServletConfig().getInitParameter("user");
-        String passwordValue = getServletConfig().getInitParameter("password");
+        String userPassword = getServletConfig().getInitParameter("password");
 
-        if (userId.equals(user) && passwordValue.equals(password)) {
+        if (userId.equals(user) && userPassword.equals(password) && Pattern.matches(userName, name)) {
             request.setAttribute("user", user);
             request.getRequestDispatcher("LoginSuccess.jsp").forward(request, response);
         } else {
             RequestDispatcher requestDispatcher = getServletContext().getRequestDispatcher("/login.html");
             PrintWriter out = response.getWriter();
-            out.println("<font color=red> Either username and password is wrong </font>");
+            out.println("<font color=red> Either username Or password is wrong </font>");
             requestDispatcher.include(request, response);
         }
     }
